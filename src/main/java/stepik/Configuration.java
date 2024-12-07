b59410301ca247a87c26cf008e61147377cf1eed
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2024-12-08 00:01:48 +0300

    Add config.properties file. Add new class Configuration

---
+++ src/main/java/stepik/Configuration.java
@@ -0,0 +1,27 @@
+package stepik;
+
+import lombok.SneakyThrows;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Configuration {
+    private static Properties properties;
+
+    public static Properties getConfiguration() {
+        return getConfiguration("config.properties");
+    }
+
+    @SneakyThrows
+    public static Properties getConfiguration(String filename) {
+        try (InputStream input = Configuration.class.getClassLoader().getResourceAsStream(filename)) {
+
+            if (input == null) {
+                throw new IllegalArgumentException("Unable to find " + filename);
+            }
+
+            properties = new Properties();
+            properties.load(input);
+        }
+        return properties;
+    }
+}
