30b5660efe58a5de81d912818752f0b0aa518516
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2024-12-08 18:00:46 +0300

    Delete class Main.java

--- src/test/java/stepik/CreateBookTest.java
+++ src/test/java/stepik/CreateBookTest.java
@@ -1,33 +1,33 @@
 package stepik;
 
-import io.restassured.http.ContentType;
+
 import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 import stepik.properties.Book;
 import stepik.properties.Category;
-import stepik.properties.TestConfig;
-
-import static io.restassured.RestAssured.given;
+import stepik.rest.TestClient;
 
 public class CreateBookTest {
 
     @Test
     public void createBookTest() {
 
+        TestClient testClient = new TestClient();
+
         Book book = new Book("The Adventures of Tom Sawyer",
                 "The story about Tom Sawyer.",
                 "Mark Twain", 350, 10, Category.Adventures);
 
 
-        when().post("books").
-        then().assertThat().
-                statusCode(201).
-                body("id", Matchers.notNullValue()).
-                body("title", Matchers.equalTo("The Adventures of Tom Sawyer")).
-                body("description", Matchers.equalTo("The story about Tom Sawyer.")).
-                body("author", Matchers.equalTo("Mark Twain")).
-                body("price", Matchers.equalTo(350)).
-                body("count", Matchers.equalTo(10)).
-                log().all();
+        testClient.createBook(book)
+                .assertThat()
+                .statusCode(201)
+                .body("id", Matchers.notNullValue())
+                .body("title", Matchers.equalTo("The Adventures of Tom Sawyer"))
+                .body("description", Matchers.equalTo("The story about Tom Sawyer."))
+                .body("author", Matchers.equalTo("Mark Twain"))
+                .body("price", Matchers.equalTo(350))
+                .body("count", Matchers.equalTo(10))
+                .log().all();
     }
 }
