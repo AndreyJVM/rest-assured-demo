4378b68556501ec6a391cb244fd4f99812c87474
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2024-12-09 16:13:54 +0300

    Add new methods createError400 -> BookResponse.java

--- src/main/java/stepik/properties/BookResponse.java
+++ src/main/java/stepik/properties/BookResponse.java
@@ -1,11 +1,14 @@
 package stepik.properties;
 
 import lombok.*;
+import lombok.experimental.Accessors;
+
 
 import java.time.OffsetDateTime;
 
 @Setter
 @Getter
+@Accessors(chain = true)
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 @NoArgsConstructor
@@ -14,4 +17,15 @@ public class BookResponse extends Book {
 
     private Integer id;
     private OffsetDateTime lastUpdated;
+
+    private OffsetDateTime timestamp;
+    private Integer status;
+    private String error;
+    private String path;
+
+    public static BookResponse createError400() {
+        return new BookResponse().setStatus(400).
+                setError("Bad Request").
+                setPath("/rest-api/books");
+    }
 }
