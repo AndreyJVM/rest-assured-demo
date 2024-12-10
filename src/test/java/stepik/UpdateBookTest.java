70eab6bc7f2d0c9bf0109ff0a4aa3a25a4edb1d3
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2024-12-10 10:52:19 +0300

    Add new test class UpdateBookTest.java

---
+++ src/test/java/stepik/UpdateBookTest.java
@@ -0,0 +1,18 @@
+package stepik;
+
+import org.testng.annotations.BeforeClass;
+import stepik.properties.Book;
+import stepik.properties.BookStoreTestCase;
+
+public class UpdateBookTest extends BookStoreTestCase {
+
+    private Integer id;
+
+    @BeforeClass
+    public void setUp() {
+        id = testClient
+                .createBook(Book.defaultOf())
+                .checkStatusCode(201)
+                .getId();
+    }
+}
