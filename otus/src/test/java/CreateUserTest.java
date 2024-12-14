da5fd725e0a2584c9e7bf40234ee31dce0d96675
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2024-12-15 01:43:00 +0300

    Add new test methods -> CreateUserTeset.java ->

--- otus/src/test/java/CreateUserTest.java
+++ otus/src/test/java/CreateUserTest.java
@@ -1,14 +1,17 @@
 import dto.UserDTO;
+import io.restassured.response.ValidatableResponse;
 import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
 import services.ServiceAPI;
+
+import static org.testng.AssertJUnit.assertEquals;
 
 public class CreateUserTest {
 
     ServiceAPI userAPI = new ServiceAPI();
 
     @Test
-    public void createUser() {
+    public void createUserAssertSC_OK() {
         UserDTO userDTO = UserDTO.builder()
                 .id(494)
                 .userStatus(505)
@@ -17,5 +20,21 @@ public class CreateUserTest {
                 .build();
         userAPI.createUser(userDTO)
                 .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    public void createUserValidateResponseMessageWithJsonPath() {
+        UserDTO userDTO = UserDTO.builder()
+                .id(494)
+                .userStatus(505)
+                .firstName("Ivan")
+                .email("example@google.com")
+                .build();
+
+        ValidatableResponse response = userAPI.createUser(userDTO);
+
+        String actualMessage = response.extract().jsonPath().get("message").toString();
+
+        assertEquals("494", actualMessage);
     }
 }
